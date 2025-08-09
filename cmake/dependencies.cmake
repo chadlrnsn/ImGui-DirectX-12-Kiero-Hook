a011e17e400c5dd0f5506b8a7d8d57d3071f374a
chadlrnsn <chadlrnsn@gmail.com>
2025-08-10 00:36:56 +0500

    refactor: build with dll

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,15 +1,5 @@
-# Подключаем FetchContent
 include(FetchContent)
 
-# Импорт ImGui
-FetchContent_Declare(
-    imgui
-    GIT_REPOSITORY https://github.com/ocornut/imgui.git
-    GIT_TAG v1.91.7
-)
-FetchContent_MakeAvailable(imgui)
-
-# Импорт MinHook
 FetchContent_Declare(
     minhook
     GIT_REPOSITORY https://github.com/TsudaKageyu/minhook.git
@@ -24,24 +14,3 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(fmt)
 
-# Создаем статическую библиотеку ImGui
-set(IMGUI_SOURCES
-    ${imgui_SOURCE_DIR}/imgui.cpp
-    ${imgui_SOURCE_DIR}/imgui_demo.cpp
-    ${imgui_SOURCE_DIR}/imgui_draw.cpp
-    ${imgui_SOURCE_DIR}/imgui_tables.cpp
-    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
-    ${imgui_SOURCE_DIR}/backends/imgui_impl_dx12.cpp
-    ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.cpp
-)
-
-add_library(imgui_lib STATIC ${IMGUI_SOURCES})
-target_include_directories(imgui_lib PUBLIC 
-    ${imgui_SOURCE_DIR}
-    ${imgui_SOURCE_DIR}/backends
-)
-
-# Настройка STB - добавляем include директорию для скачанных файлов
-target_include_directories(imgui_lib PUBLIC 
-    ${CMAKE_CURRENT_BINARY_DIR}
-) 
